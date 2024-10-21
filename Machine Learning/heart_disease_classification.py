84059a1d93d0da1e4da49deb70b3f0089252b11c
Rahul VadiSetty <vadisettyrahul@gmail.com>
2024-10-21 06:52:21 -0700

    Update heart_disease_classification.py

--- Machine Learning/heart_disease_classification.py
+++ Machine Learning/heart_disease_classification.py
@@ -20,3 +20,14 @@ data.head()
 X=data.drop('HeartDisease',axis=1)
 y=data['HeartDisease']
 
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+
+model=RandomForestClassifier(n_estimators=15)
+
+model.fit(X_train,y_train)
+
+print('Accuracy Score:',model.score(X_test,y_test))
